383f7ffbc346247f735c49e570050d8fd3fbb115
Shubham Singh <shubhamsingh1840@gmail.com>
2020-06-02 15:10:17 +0530

    updated

--- tree/questions.java
+++ tree/questions.java
@@ -185,4 +185,45 @@ public class questions {
         }
         return true;
     }
+
+    // ===============leetcode 617==============
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null)
+            return null;
+
+        int far = 0;
+        if (t1 != null && t2 != null) {
+            far = t1.val + t2.val;
+            TreeNode my = new TreeNode(far);
+            my.left = mergeTrees(t1.left, t2.left);
+            my.right = mergeTrees(t1.right, t2.right);
+            return my;
+        } else if (t1 == null) {
+            far = t2.val;
+            TreeNode my = new TreeNode(far);
+            my.left = mergeTrees(null, t2.left);
+            my.right = mergeTrees(null, t2.right);
+            return my;
+        } else {
+            far = t1.val;
+            TreeNode my = new TreeNode(far);
+            my.left = mergeTrees(t1.left, null);
+            my.right = mergeTrees(t1.right, null);
+            return my;
+        }
+    }
+
+    // alternate solution
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null)
+            return t2;
+        if (t2 == null)
+            return t1;
+        t1.val += t2.val;
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+        return t1;
+    }
+
+    
 }
