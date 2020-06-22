0005bbd685da13621837cfa5082de82b6fd21417
Shubham <shubhamsingh1840@gmail.com>
2020-06-22 20:11:36 +0530

    updated

--- stacks/questions.java
+++ stacks/questions.java
@@ -91,6 +91,125 @@ public class questions {
         return ans;
     }
 
+    // leetcode 901
+
+    class StockSpanner {
+        Stack st;
+        int i;
+
+        public class pair {
+            int idx = 0;
+            int val = 0;
+
+            pair(int idx, int val) {
+                this.idx = idx;
+                this.val = val;
+            }
+        }
+
+        public StockSpanner() {
+            st = new Stack();
+            st.push(new pair(-1, -1));
+            i = 0;
+        }
+
+        public int next(int price) {
+            int ans = 1;
+            while (((pair) st.peek()).idx != -1 && ((pair) st.peek()).val <= price) {
+                st.pop();
+            }
+            ans = i - ((pair) st.peek()).idx;
+            st.push(new pair(i, price));
+            i++;
+            return ans;
+        }
+    }
+
+    // leetcode 921
+    public int minAddToMakeValid(String str) {
+        Stack st = new Stack();
+        st.push(-1);
+        for (int i = 0; i < str.length(); i++) {
+            if ((int) st.peek() != -1 && str.charAt((int) st.peek()) == '(' && str.charAt(i) == ')')
+                st.pop();
+
+            else
+                st.push(i);
+        }
+        return st.size() - 1;
+    }
+
+    public int minAddToMakeValid_02(String str) {
+        int openningBracReq = 0;
+        int closingBracReq = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(')
+                closingBracReq++;
+            else if (str.charAt(i) == ')' && closingBracReq >= 1)
+                closingBracReq--;
+            else
+                openningBracReq++;
+        }
+        return openningBracReq + closingBracReq;
+    }
+
+    // leetcode 1249
+    public String minRemoveToMakeValid(String str) {
+        StringBuilder sb = new StringBuilder();
+        Stack st = new Stack();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '(') {
+                sb.append('(');
+                st.push(i);
+            } else if (ch == ')') {
+                if (st.size() != 0 && str.charAt((int) st.peek()) == '(' && ch == ')') {
+                    sb.setCharAt((int) st.peek(), '[');
+                    sb.append(']');
+                    st.pop();
+                } else
+                    sb.append(ch);
+            } else {
+                sb.append(ch);
+            }
+        }
+        StringBuilder ans = new StringBuilder();
+        String temp = sb.toString();
+        for (int i = 0; i < temp.length(); i++) {
+            char ch = temp.charAt(i);
+            if (ch == '[')
+                ans.append("(");
+            else if (ch == ']')
+                ans.append(")");
+            else if (ch >= 'a' && ch <= 'z')
+                ans.append(ch);
+        }
+        return ans.toString();
+    }
+
+    public String minRemoveToMakeValid_02(String str) {
+        StringBuilder sb = new StringBuilder(str);
+        Stack st = new Stack();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch >= 'a' && ch <= 'z')
+                continue;
+            if (st.size() != 0 && str.charAt((int) st.peek()) == '(' && ch == ')')
+                st.pop();
+            else
+                st.push(i);
+        }
+        while (!st.isEmpty()) {
+            int idx = (int) st.pop();
+            sb.setCharAt(idx, '*');
+        }
+        String ans = "";
+        for (int i = 0; i < sb.length(); i++) {
+            if (sb.charAt(i) != '*')
+                ans += sb.charAt(i);
+        }
+        return ans;
+    }
 
     
 }
